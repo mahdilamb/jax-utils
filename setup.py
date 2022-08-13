be63102b99399e5dceb109ed8ab4dd62a1b80777
mahdilamb <mahdilamb@gmail.com>
2022-08-13 13:41:41 +0100

    Try to package again

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-import pathlib
-from typing import List
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='jax_utils',
     version='0.0.1',
-    packages=find_packages(include=['jax_utils', 'jax_utils.*']),
-    install_requires=[
+    packages=('jax_utils',),
+    install_requires=(
         'jax',
-    ],
+    )
+    ,
 )
